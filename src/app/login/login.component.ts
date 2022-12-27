42f8d1fe595077abb6dfd0c67cb35386146c9597
Anagha K M <anaghakm00@gmail.com>
2022-12-27 23:27:27 +0530

    BankApp

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit{ //3rd executed
+  constructor() {  //1st executed 
+  //constructor - used for object initialization
+  //it automatically invokes when an object is created
+  }
+  ngOnInit(): void { //2nd executed
+  //initial process of a component
+  //when a component is created, at same time it initialize or authorize
+  //when a component is created, there is a lifecycle for it
+    
+  }
+
+  //properties
+  aim="Your Perfect Banking Partner"
+  account="Please Enter Your Account Number"
+  acno:any; //to hold value from the user
+  pswd:any;
+  
+
+
+  userDetails:any={
+    1000:{acno:1000, username:"Anagha", password:1000, balance:2000},
+    1001:{acno:1001, username:"Rithu", password:1001, balance:2000},
+    1002:{acno:1002, username:"Jishna", password:1002, balance:2000},
+    
+  }
+  //userdefined functions - 4th executed
+  login(){
+    var acno=this.acno;
+    var pswd=this.pswd;
+    var userDetails=this.userDetails;
+
+    if(acno in userDetails){
+      if(pswd==userDetails[acno]['password']){
+        alert('Login successful')
+      }else{
+        alert('Incorrect password')
+      }
+    }else{
+      alert('user not found')
+    }
+    // alert('Login Clicked');
+  }
+
+
+  acnoChange(event:any){
+    // console.log(event.target.value);
+    this.acno = event.target.value;
+    console.log(this.acno);
+    
+  }
+  pswdChange(event:any){
+    this.pswd = event.target.value;
+    console.log(this.pswd);
+    
+    
+  }
+  
+
+}
